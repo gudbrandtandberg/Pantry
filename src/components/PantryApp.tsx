e603c32ab4c3f213baeddbbf9ba82e615adf75ae
Gudbrand Tandberg <gudbrandduff@gmail.com>
2025-02-25 20:35:17 +0100

    Remove src dir

--- src/components/PantryApp.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from 'react';
-import { usePantry } from '../contexts/PantryContext';
-
-// Main component - similar to a Python class that manages the UI
-export default function PantryApp() {
-    const { currentPantry } = usePantry();
-    // ... renders the UI based on current state
-} 
